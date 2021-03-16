7c7b2a1b98cdf9b4eacc37632c6976f17a03522f
David Grijalva <grijalva789@gmail.com>
2021-03-16 18:43:30 -0500

    integrate graph

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask, render_template, request
 from forms import WriteYourOwnQuery, MinimumUserInput
 from utils import handle_custom_form, handle_minimum_user_input_form, handle_query, list_of_customizable_queries, query_string
 import altair as alt
-
+import pandas as pd
+import json
 
 app = Flask(__name__)
 app.secret_key = 'development_key'
@@ -39,8 +40,17 @@ def customizable():
         sql_query = list_of_customizable_queries(sql_type, sql_sort, sql_limit, sql_string_value)
         data = handle_query(sql_query)
         string_q = query_string(sql_sort, sql_limit, sql_type, sql_string_value)
+        df = pd.DataFrame({
+            "x": ['A', 'B', 'C', 'D', 'E'],
+            "y": [5, 6, 7, 8, 9]})
+        chart = alt.Chart(df).mark_bar().encode(
+            x='x',
+            y='y',
+        )
+        chart_json = chart.to_dict()
+        print(chart_json)
         return render_template("index.html", data=data, form_write_your_own_query=form_write_your_own_query,
-                               form_customizable=form_customizable, string_q=string_q)
+                               form_customizable=form_customizable, string_q=string_q, chart=chart_json)
 
 
 if __name__ == '__main__':
